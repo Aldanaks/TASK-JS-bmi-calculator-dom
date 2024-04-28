51b0c1d17ace2690b297c9cecac4bbd5942b2c5c
Aldanaks <aldanakhs@gmail.com>
2024-04-28 20:04:26 +0300

    .

--- index.js
+++ index.js
@@ -8,4 +8,31 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+
+  let BMI = weight / ((height / 100) * (height / 100));
+
+  alert(BMI);
+
+  if (BMI < 18.5) {
+    alert("Underweight");
+  } else if (BMI >= 18.5 && BMI <= 24.9) {
+    alert("Healthy Weihght");
+  } else if (BMI >= 25.5 && BMI <= 29.9) {
+    alert("Overweight");
+  } else if (BMI > 30) {
+    alert("Obesity");
+  }
+
+  if (age >= 19 && age <= 24 && BMI >= 19 && BMI >= 24) {
+    alert("Healthy");
+  } else if (age >= 25 && age <= 34 && BMI >= 20 && BMI <= 25) {
+    alert("Healthy");
+  } else if (age >= 35 && age <= 44 && BMI >= 21 && BMI <= 26) {
+    alert("Healthy");
+  } else if (age >= 45 && age <= 54 && BMI >= 22 && BMI <= 27) {
+    alert("Healthy");
+  } else if (age >= 55 && age <= 64 && BMI >= 23 && BMI <= 29) {
+    alert("Healthy");
+  } else if (age > 65 && BMI >= 24 && BMI <= 29) alert("Healthy");
+  else alert("Not Healthy");
 }
